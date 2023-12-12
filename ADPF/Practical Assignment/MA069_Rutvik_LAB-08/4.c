e8d8c498a523f809935f5be47749abec42f47266
rutvik966 <ghub007700@gmail.com>
2023-12-12 15:53:44 +0530

    Hello

--- ADPF/Practical Assignment/MA069_Rutvik_LAB-08/4.c
+++ ADPF/Practical Assignment/MA069_Rutvik_LAB-08/4.c
@@ -6,44 +6,54 @@ double len(int x1, int y1, int x2, int y2) {
 }
 
 double area(double s1, double s2, double s3) {
-    double s = (s1 + s2 + s3) / 2.0;
+    double s = (s1 + s2 + s3) / 2;
     return sqrt(s * (s - s1) * (s - s2) * (s - s3));
 }
 
-int check(int x[], int y[], int nx, int ny) {
-    double t = area(len(x[0], y[0], x[1], y[1]), len(x[1], y[1], x[2], y[2]), len(x[2], y[2], x[0], y[0]));
-    double s1 = area(len(nx, ny, x[0], y[0]), len(x[1], y[1], x[2], y[2]), len(nx, ny, x[1], y[1]));
-    double s2 = area(len(nx, ny, x[0], y[0]), len(x[1], y[1], x[2], y[2]), len(nx, ny, x[2], y[2]));
-    double s3 = area(len(nx, ny, x[1], y[1]), len(x[0], y[0], x[2], y[2]), len(nx, ny, x[2], y[2]));
+int check(int xs[], int ys[], int nx, int ny) {
+    double t = area(len(xs[0], ys[0], xs[1], ys[1]),
+                    len(xs[1], ys[1], xs[2], ys[2]),
+                    len(xs[2], ys[2], xs[0], ys[0]));
 
-    return t == s1 + s2 + s3;
+    double a1 = area(len(nx, ny, xs[1], ys[1]),
+                     len(xs[1], ys[1], xs[2], ys[2]),
+                     len(xs[2], ys[2], nx, ny));
+
+    double a2 = area(len(xs[0], ys[0], nx, ny),
+                     len(nx, ny, xs[2], ys[2]),
+                     len(xs[2], ys[2], xs[0], ys[0]));
+
+    double a3 = area(len(xs[0], ys[0], xs[1], ys[1]),
+                     len(xs[1], ys[1], nx, ny),
+                     len(nx, ny, xs[0], ys[0]));
+
+    return t == a1 + a2 + a3;
 }
 
 int main() {
     int x[3], y[3];
-    for (int i = 0; i < 3; ++i) {
-        printf("Enter coordinates for point %d (x y): ", i + 1);
+    for (int i = 0; i < 3; ++i)
         scanf("%d %d", &x[i], &y[i]);
-    }
 
     double s1 = len(x[0], y[0], x[1], y[1]);
     double s2 = len(x[1], y[1], x[2], y[2]);
     double s3 = len(x[2], y[2], x[0], y[0]);
 
     if (s1 + s2 > s3 && s2 + s3 > s1 && s3 + s1 > s2) {
-        printf("Triangle can be formed.\n");
-        printf("Area: %f\n", area(s1, s2, s3));
+        printf("1\n");
+
+        double a = area(s1, s2, s3);
+        printf("%f\n", a);
 
         int nx, ny;
-        printf("Enter coordinates for a new point (x y): ");
         scanf("%d %d", &nx, &ny);
 
         if (check(x, y, nx, ny))
-            printf("The point is inside the triangle.\n");
+            printf("1\n");
         else
-            printf("The point is outside the triangle.\n");
+            printf("0\n");
     } else {
-        printf("Triangle cannot be formed with the given points.\n");
+        printf("0\n");
     }
 
     return 0;
